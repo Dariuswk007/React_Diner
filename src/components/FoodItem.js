011dee2a73a6ab4ed7f1675f0ae41dc50dd39976
Darius007 <dariuswk@aol.com>
2022-08-04 19:32:00 -0500

    inital commit

---
+++ src/components/FoodItem.js
@@ -0,0 +1,23 @@
+
+import React, { useState } from "react";
+
+const FoodItem = (props) => {
+  const { title, price } = props.FoodItem;
+  const [clicked, setClicked] = useState("");
+
+  return (
+    <div
+      className="foodItem-container"
+      onClick={() => {
+        // setClicked("clicked");
+        return props.selectFoodItem(props.FoodItem);
+      }}
+    >
+      <h3 className={`food-display`}>
+        {title}: <span className="price-tag">${price}</span>
+      </h3>
+    </div>
+  );
+};
+
+export default FoodItem;
